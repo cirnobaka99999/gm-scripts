5321700e1a3ac61a8859677757fdf35b6f776671
Seiya Loveless <seiya-git@users.noreply.github.com>
2015-11-09 03:31:39 +0300

    Update Rutracker.org_hash_downloader.user.js
    new trackers

--- Rutracker.org_hash_downloader.user.js
+++ Rutracker.org_hash_downloader.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Rutracker.org hash downloader 
-// @version     0.7
+// @version     1.0
 // @namespace   http://vk.com/seiya_loveless?#Rutracker.org_hash_downloader.user.js
 // @description Download torrent from rutracker.org via hash
 // @include     http://rutracker.org/forum/viewtopic.php?t=*
@@ -18,6 +18,10 @@ $(function(){
 		hash.string = $('#tor-hash').text();
 		hash.title = $('#topic-title').text();
 		hash.uri  = 'magnet:?xt=urn:btih:'+hash.string+'&dn='+encodeURIComponent(hash.title);
+			hash.uri += '&tr='+encodeURIComponent('http://bt.rutracker.cc/ann');
+			hash.uri += '&tr='+encodeURIComponent('http://bt2.rutracker.cc/ann');
+			hash.uri += '&tr='+encodeURIComponent('http://bt3.rutracker.cc/ann');
+			hash.uri += '&tr='+encodeURIComponent('http://bt4.rutracker.cc/ann');
 			hash.uri += '&tr='+encodeURIComponent('udp://tracker.openbittorrent.com:80/announce');
 			hash.uri += '&tr='+encodeURIComponent('udp://tracker.publicbt.com:80/announce');
 			hash.uri += '&tr='+encodeURIComponent('udp://open.demonii.com:1337/announce');
